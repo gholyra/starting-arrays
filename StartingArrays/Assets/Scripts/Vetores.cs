97b2f5a050516bf969f874f26e2b33eac7475044
Tiago Marchetti Lyra <tiago.mlyra@gmail.com>
2023-04-10 20:59:42 -0300

    2º Commit do Projeto
    Projeto realizado na aula do dia 10/04, onde realizamos o estudo de vetores e listas, basicamente os princípios e utilidades de array, usando C# no Visual Studio.

--- StartingArrays/Assets/Scripts/Vetores.cs
+++ StartingArrays/Assets/Scripts/Vetores.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,23 +7,51 @@ public class Vetores : MonoBehaviour
 {
 
     private string[] personagens = new string[] {"Baldur", "Anduim", "Rexxar", "Frodo", "Kratos"};
-    private int[] numeros = new int[5];
-    private bool[] booleanos = new bool[5] {true, false, false, true, true} ;
+    private string[] arsenalArmas = new string[10];
+    [SerializeField] private string[] armas;
+    [SerializeField] private string[] classes;
+    //private int[] numeros = new int[5];
+    //private bool[] booleanos = new bool[5] {true, false, false, true, true} ;
 
     // Start is called before the first frame update
     void Start()
     {
-        numeros[0] = 5;
-        numeros[1] = 10;
-        numeros[2] = 1;
-        numeros[3] = 7;
-        numeros[4] = 32;
+        //print(personagens[1]);
+        //personagens[1] = "CJ";
+        //print(personagens[1]);
+        //numeros[0] = 5;
+        //numeros[1] = 10;
+        //numeros[2] = 1;
+        //numeros[3] = 7;
+        //numeros[4] = 32;
+        AdicionarArmasNoInventario(armas);
 
-        for (int i = 0; i < personagens.Length; i++)
+        //for (int i = 0; i < personagens.Length; i++)
+        //{
+        //    print(personagens[i]);
+        //}
+        //foreach (string personagem in personagens)
+        //{
+        //    print(personagem);
+        //}
+
+    }
+
+    private void AdicionarArmasNoInventario(string[] armas)
+    {
+        for (int i = 0; i < armas.Length; i++)
         {
-            print(personagens[i]);
+            arsenalArmas[i] = armas[i];
+            //print("A arma " + armas[i] + " está sendo adicionada ao arsenal de armas.");
+            print($"A arma {armas[i]} está sendo adicionada ao arsenal de armas.");
         }
-        
+
+        print("Este é o seu arsenal de armas atual.");
+
+        for (int i = 0; i < arsenalArmas.Length; i++)
+        {
+            print(arsenalArmas[i]);
+        }
 
     }
 
